3d97f9d694694a348367a699913c12af1bfea369
Nurus Shakib Rhythm <nurusshakib88@gmail.com>
2024-12-20 03:15:43 +0600

    test2

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { useContext } from "react";
+import { AuthContext } from "../contexts/AuthContext";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return <div>Home</div>;
+  const { auth } = useContext(AuthContext);
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="font-bold text-5xl uppercase">WELCOME to mess meal</h1>
+
+      <button className="btn btn-primary my-5">
+        {auth ? (
+          <Link to="/dashboard">Dashboard</Link>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
+      </button>
+    </div>
+  );
 };
 
 export default Home;
