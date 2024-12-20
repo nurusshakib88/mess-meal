d27d08ac864eac42b6edc9892dbcd9475d714a2b
Nurus Shakib Rhythm <nurusshakib88@gmail.com>
2024-12-20 22:36:55 +0600

    test final

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 
 // Enable CORS
 const corsOptions = {
-  origin: "https://mess-meal.vercel.app", // Replace with your frontend URL
+  origin: "*", // Replace with your frontend URL
   credentials: true, // Allow credentials (like cookies and authorization headers)
   methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"], // Explicitly allow PATCH
   allowedHeaders: ["Content-Type", "Authorization"], // Allow headers
